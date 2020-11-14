5f3e28996117bd14863e17001a99af49276ee72a
jameswingert <jameswingert1@gmail.com>
2020-11-14 02:49:56 -0500

    Fixed reset bug

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
 const p2Display = document.querySelector('#p2Display');
 const p1Display = document.querySelector('#p1Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -11,8 +13,10 @@ let isGameOver = false;
 p1Button.addEventListener('click', function() {
 	if (!isGameOver) {
 		p1Score += 1;
-		if (p1Score == winningScore) {
+		if (p1Score === winningScore) {
 			isGameOver = true;
+			p1Display.classList.add('winner');
+			p2Display.classList.add('loser');
 		}
 		p1Display.textContent = p1Score;
 	}
@@ -20,10 +24,26 @@ p1Button.addEventListener('click', function() {
 p2Button.addEventListener('click', function() {
 	if (!isGameOver) {
 		p2Score += 1;
-		if (p2Score == winningScore) {
+		if (p2Score === winningScore) {
 			isGameOver = true;
+			p2Display.classList.add('winner');
+			p1Display.classList.add('loser');
 		}
 		p2Display.textContent = p2Score;
 	}
 });
-test;
+
+winningScoreSelect.addEventListener('change', function() {
+	winningScore = parseInt(this.value);
+	reset();
+});
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+	isGameOver = false;
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+}
